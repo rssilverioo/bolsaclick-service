7b7514da66f999b6b810d161fb7ffab3006445a2
Rodrigo Soares Silverio <rodrigo.silverio@compasso.com.br>
2025-06-02 00:55:33 -0300

    fix: ensure all Redis and App controllers are deployed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './redis/redis.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     RedisModule,
   ],
+  controllers: [AppController], // <-- ISSO É ESSENCIAL!
 })
 export class AppModule {}
