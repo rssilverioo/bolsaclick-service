fc448f1a2444af37ce0fe5be356a16098eb90ae8
Rodrigo Silverio <rodrigo.silverio@compasso.com.br>
2025-06-04 11:45:37 -0300

    update headers api KEY

--- src/partner/anhanguera/anhanguera.controller.ts
+++ src/partner/anhanguera/anhanguera.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UnauthorizedException,
+  Headers,
+} from '@nestjs/common';
 import { AnhangueraService } from './anhanguera.service';
 import { ApiExcludeController } from '@nestjs/swagger';
 
@@ -8,8 +13,12 @@ export class AnhangueraController {
   constructor(private readonly anhangueraService: AnhangueraService) {}
 
   @Post('sync-all')
-  async syncAll() {
+  async syncAll(@Headers('x-api-key') apiKey: string) {
+    if (apiKey !== process.env.CRON_API_KEY) {
+      throw new UnauthorizedException('Invalid API Key');
+    }
+
     await this.anhangueraService.syncAllOffers();
-    return { success: true };
+    return { success: true, message: 'Sync completed via GitHub Actions' };
   }
 }
